64a4e756f9ffe92c0ae6132e795e33365916e145
tphlru <35965490+tphlru@users.noreply.github.com>
2024-08-04 01:36:00 +0300

    Optimise namespacing

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,4 +1,4 @@
-from rzd_detector.codemodules.face.pulse.pulse import get_bpm_with_pbv
+from rzd_detector.codemodules.face.pulse import get_bpm_with_pbv
 
 
 result = get_bpm_with_pbv("Scripts/test_files/pulse/zlata83.mp4")
---
+++ src/codemodules/face/pulse/__init__.py
@@ -0,0 +1,7 @@
+# Just some namespacing
+
+from .pulse import get_bpm_with_pbv
+
+__all__ = [
+    "get_bpm_with_pbv",
+]
