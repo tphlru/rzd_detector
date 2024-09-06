20bcd2e6acd6e1f160a486c7a8f1047f9b4db159
tphlru <35965490+tphlru@users.noreply.github.com>
2024-09-06 23:08:36 +0300

    Add cornernet tools

--- src/rzd_detector/common/utils.py
+++ src/rzd_detector/common/utils.py
@@ -1,7 +1,41 @@
 import numpy as np
 from typing import TypeVar, Iterable, Union
+import shutil
+import os
+import git
 
 T = TypeVar("T", bound=Iterable)
+
+
+def clone_and_extract(
+    repo_url: str,
+    target_dir: str,
+    extract_dir: str,
+    branch: str = "main",
+):
+    """Функция, чтобы скачать репозиторий и вытащить из него конкретную директторию
+
+    Args:
+        repo_url (str): Ссылка в формате https://github.com/user/repo.git
+        branch (str): Ветка репозитория, по умолчанию main
+        target_dir (str): В какую локальную директорию извлечь цель
+        extract_dir (str): Путь в репозитории, который надо извлечь
+
+    Raises:
+        ValueError: Если нет такой директории в репозитории
+    """
+    # Клонируем репозиторий в временную директорию
+    temp_dir = "temp_repo"
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+    git.Repo.clone_from(repo_url, temp_dir, branch=branch, depth=1)
+    src_dir = os.path.join(temp_dir, extract_dir)
+    if not os.path.isdir(src_dir):
+        raise ValueError(f"Directory {src_dir} does not exist in the repository.")
+    if os.path.exists(target_dir):
+        shutil.rmtree(target_dir)  # Удаляем целевую директорию, если она существует
+    shutil.copytree(src_dir, target_dir)
+    shutil.rmtree(temp_dir)
 
 
 def add_offset_to_values(data: T, offset: Union[str, int, float]) -> T:
