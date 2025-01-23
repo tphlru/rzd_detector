dfda6f0f05b82bde18f1b67e6ef01af231478e1e
Oedada <163449434+Oedada@users.noreply.github.com>
2025-01-23 23:27:35 +0300

    adding a square bracket bug fix

--- src/rzd_detector/codemodules/behaviour/clothes/clothes_segmentation/train_model.py
+++ src/rzd_detector/codemodules/behaviour/clothes/clothes_segmentation/train_model.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640, save=True)
+results = model.train(data=r"C:\Users\Georges\Projects\datasets\DeepFashion2_YOLO\data.yml", epochs=100, imgsz=640, save=True)
