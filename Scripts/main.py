022eddd41b4416ab59baa95a687d68dcb181b4c8
Oedada <163449434+Oedada@users.noreply.github.com>
2025-02-21 02:02:02 +0300

    start finalize voice module

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,11 +1,13 @@
 # emotions - :(
-# pulse
-# respiration
-# voice
+# pulse - ✓
+# respiration - ✓(нет экстраполяции)
+# voice - 
 from rzd_detector.codemodules.face.pulse import get_bpm_with_pbv, process_pulse_info, evaluate_pulse_results
-from rzd_detector.codemodules.face.respiration import 
+from rzd_detector.codemodules.face.respiration import get_resp
 def main(cuda: bool):
     bvp, times, bpm = get_bpm_with_pbv(videoFileName="Scripts\test_files\common\example1.mp4", cuda=cuda)
     base_mean, trend, high_med, mids, tvals = process_pulse_info(bvp, show_plot=False)
     pulse_points = evaluate_pulse_results(base_mean=base_mean, trend=trend, high_med=high_med, midpoints=mids, tops_values=tvals)
+
+    resp_rate, resp_points = get_resp(8, 3) # значения на рандом
 
--- src/rzd_detector/codemodules/face/respiration/get_resp.py
+++ src/rzd_detector/codemodules/face/respiration/get_resp.py
@@ -1,10 +1,12 @@
 from respiration.mttscan.predict_vitals import predict_vitals
 from common.utils import get_plot_tops_n_times
+import numpy as np
+
 def get_resp(iter_for_maxs: int, maxs_treshold: int):
     '''Объеденяет весь модуль.
     Args:
         iter_for_maxs (int): Число итераций, при поиске верхушек графика дыхания (вздохов).
-        maxs_treshold (int): Порог ниже, которого к значениям применятеся экстраполяция.
+        maxs_treshold (int): Порог количества пиков (вздохов) ниже, которого к значениям применятеся экстраполяция.
     Returns:
         tu: частота дыхания, оценка дыхания.
     '''
@@ -15,8 +17,13 @@ def get_resp(iter_for_maxs: int, maxs_treshold: int):
         resp_rate = len(ids_tops)*60/duration # подставляем новые значения пиков и новое время(надо подумать как его вычислять, это вроде как проблема), а потом вычисляем новое значение частоты дыхания
     resp_rate = len(ids_tops)*60/duration
     points = 0 
-    if 16 < resp_rate < 20:
+    if 12 < resp_rate < 20:
         points += 1
-    else:
+    elif resp_rate < 8:
         points -= 1
+    elif resp_rate > 25:
+        points -= 1
+    if max(vals_tops) / np.mean(vals_tops) > 1.5:
+        points -= 1
+    
     return resp_rate, points
